574afd082c09b22ca37249e30c5675f51e95117f
Jefersson Nathan <admin@phpse.net>
2015-05-02 17:33:11 -0300

    Clear load services config

--- src/Conticket/ApiBundle/DependencyInjection/ApiExtension.php
+++ src/Conticket/ApiBundle/DependencyInjection/ApiExtension.php
@@ -8,9 +8,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * @author Jefersson Nathan <malukenho@phpse.net>
  */
 class ApiExtension extends Extension
 {
@@ -19,11 +17,7 @@ class ApiExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-//        $loader->load('services.yml');
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-//        $loader->load('domain.xml');
     }
 }
