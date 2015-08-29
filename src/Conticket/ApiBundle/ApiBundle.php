000548a1c18b3c74bef08f7d1191b31322201663
Jefersson Nathan <admin@phpse.net>
2015-08-29 04:21:59 -0300

    Added author tag

--- src/Conticket/ApiBundle/ApiBundle.php
+++ src/Conticket/ApiBundle/ApiBundle.php
@@ -4,6 +4,9 @@ namespace Conticket\ApiBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * @author Jefersson Nathan <malukenho@phpse.net>
+ */
 final class ApiBundle extends Bundle
 {
 }
