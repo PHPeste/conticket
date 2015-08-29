f8e41cd28eddd31674091d056a96b639d92cbc6c
Jefersson Nathan <admin@phpse.net>
2015-08-29 04:46:04 -0300

    Refactor event controller

--- src/Conticket/ApiBundle/Controller/EventController.php
+++ src/Conticket/ApiBundle/Controller/EventController.php
@@ -2,19 +2,53 @@
 
 namespace Conticket\ApiBundle\Controller;
 
+use Broadway\CommandHandling\CommandBusInterface;
+use Broadway\UuidGenerator\UuidGeneratorInterface;
+use Conticket\ApiBundle\Command\CreateNewEvent;
+use Conticket\ApiBundle\EventId;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @author  Jefersson Nathan <malukenho@phpse.net>
+ */
 final class EventController extends Controller
 {
+    /**
+     * @var CommandBusInterface
+     */
+    private $commandBus;
+    /**
+     * @var UuidGeneratorInterface
+     */
+    private $uuidGenerator;
+
+    /**
+     * @param CommandBusInterface    $commandBus
+     * @param UuidGeneratorInterface $uuidGenerator
+     */
+    public function __construct(CommandBusInterface $commandBus, UuidGeneratorInterface $uuidGenerator)
+    {
+        $this->commandBus    = $commandBus;
+        $this->uuidGenerator = $uuidGenerator;
+    }
+
     public function indexAction()
     {
-//        $em = $this->getDoctrine()->getManager();
-
         return $this->render('ApiBundle:Default:index.html.twig');
     }
 
-    public function createAction()
+    public function createAction(Request $request)
     {
-        exit('adsas');
+        $title       = $request->request->get('title');
+        $description = $request->request->get('description');
+
+        $eventId = new EventId($this->uuidGenerator->generate());
+        $command = new CreateNewEvent($eventId, $title, $description);
+
+        $this->commandBus->dispatch($command);
+
+        return new JsonResponse(['id' => (string) $eventId]);
     }
 }
