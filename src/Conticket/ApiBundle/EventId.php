8fb59bd33bf3ca2c51bb3459738447d0994dc03f
Jefersson Nathan <admin@phpse.net>
2015-08-29 11:04:29 -0300

    Implement eventId

--- src/Conticket/ApiBundle/EventId.php
+++ src/Conticket/ApiBundle/EventId.php
@@ -1,15 +1,30 @@
 <?php
-/**
- * EventId.php
- *
- * @package conticket
- * @author  Jefersson Nathan <malukenho@phpse.net>
- */
 
 namespace Conticket\ApiBundle;
 
-
-class EventId
+/**
+ * @author  Jefersson Nathan <malukenho@phpse.net>
+ */
+final class EventId implements Identifier
 {
+    /**
+     * @var string
+     */
+    private $eventId;
 
+    /**
+     * @param string $eventId
+     */
+    public function __construct($eventId)
+    {
+        $this->eventId = $eventId;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->eventId;
+    }
 }
