8001ae640ccb7078f794dcacfe856cc452d09c9a
Jefersson Nathan <admin@phpse.net>
2015-11-26 21:15:33 -0300

    Disable redis session as it's not installed on VM

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,7 +34,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
-            new Snc\RedisBundle\SncRedisBundle(),
+//            new Snc\RedisBundle\SncRedisBundle(),
             new AppBundle\AppBundle(),
             new Conticket\ApiBundle\ConticketApiBundle(),
         ];
