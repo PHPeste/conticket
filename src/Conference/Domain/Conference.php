d9da9dbcf7e615f4defb01b57f11296f6e47d75d
Jefersson Nathan <admin@phpse.net>
2017-02-03 16:23:50 -0300

    add conference aggregate

---
+++ src/Conference/Domain/Conference.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conticket\Conference;
+
+use Conticket\Conference\Domain\ConferenceId;
+use Conticket\Conference\Domain\Event\ConferenceWasCreated;
+use Prooph\EventSourcing\AggregateRoot;
+
+/**
+ * @author Jefersson Nathan <malukenho@phpse.net>
+ */
+final class Conference extends AggregateRoot
+{
+    /**
+     * @var ConferenceId
+     */
+    private $conferenceId;
+
+    public static function new(
+        ConferenceId $conferenceId,
+        string $name,
+        string $description,
+        string $author,
+        \DateTimeImmutable $date
+    ): self {
+        $self = new self();
+        $self->recordThat(ConferenceWasCreated::fromRequestData($conferenceId, $name, $description, $author, $date));
+
+        return $self;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function aggregateId(): string
+    {
+        return (string) $this->conferenceId;
+    }
+}
