34c58521e1805e73b855073d8c3a11d1310c01c9
Jefersson Nathan <admin@phpse.net>
2017-02-03 16:24:41 -0300

    describe services on config file

--- config/services.php
+++ config/services.php
@@ -1,6 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
+use Conference\Domain\Repository\ConferenceRepositoryInterface;
+use Conference\Factory\Repository\ConferenceRepositoryFactory;
+use Conticket\Conference\Factory\CommandHandler\CreateConferenceHandlerFactory;
+use Conticket\Conference\Domain\Command\CreateConference;
+use Conticket\Conference\Infrastructure\Service\CommandBusFactory;
 use Interop\Container\ContainerInterface;
+use Prooph\ServiceBus\CommandBus;
 use Zend\Expressive\Application;
 use Zend\Expressive\Router\FastRouteRouter;
 
@@ -14,6 +22,14 @@ return (function () {
             FastRouteRouter::class => function (ContainerInterface $container) {
                 return new FastRouteRouter();
             },
+
+            CommandBus::class => CommandBusFactory::class,
+
+            // @todo move commands/events to another config file
+            CreateConference::class => CreateConferenceHandlerFactory::class,
+
+            // @todo move repository to another file
+            ConferenceRepositoryInterface::class => ConferenceRepositoryFactory::class,
         ]
     ];
 })();
