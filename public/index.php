5a56cedbd74050bf5419bba61f642d33299bd3c4
Jefersson Nathan <admin@phpse.net>
2017-02-03 13:40:00 -0300

    register router on front controller

--- public/index.php
+++ public/index.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
+use Conticket\Conference\Infrastructure\Middleware\CreateAConference;
+
 (function () {
     require __DIR__ . '/../vendor/autoload.php';
 
     /* @var $serviceManager \Zend\ServiceManager\ServiceManager */
     $serviceManager = require __DIR__ . '/../config/service-manager.php';
 
+    /* @var $app \Zend\Expressive\Application */
     $app = $serviceManager->get(\Zend\Expressive\Application::class);
 
+    // @todo change it to POST
+    $app->get(CreateAConference::PATH, CreateAConference::class);
+
+    $app->pipeRoutingMiddleware();
+    $app->pipeDispatchMiddleware();
+    $app->run();
 })();
