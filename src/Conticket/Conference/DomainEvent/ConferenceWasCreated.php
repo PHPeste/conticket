0654cbe11558e6cbfca31c7bf449860d059a2b15
Jefersson Nathan <admin@phpse.net>
2017-02-03 13:50:11 -0300

    added event to create conference

---
+++ src/Conticket/Conference/DomainEvent/ConferenceWasCreated.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Conticket\Conference\DomainEvent;
+
+use Assert\Assertion;
+use Conticket\Conference\ConferenceId;
+use Prooph\EventSourcing\AggregateChanged;
+
+/**
+ * @author Jefersson Nathan <malukenho@phpse.net>
+ */
+final class ConferenceWasCreated extends AggregateChanged
+{
+    /**
+     * @var ConferenceId
+     */
+    private $conferenceId;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $author;
+
+    /**
+     * @var \DateTimeImmutable
+     */
+    private $date;
+
+    public static function fromRequestData(
+        ConferenceId $conferenceId,
+        string $name,
+        string $description,
+        string $author,
+        \DateTimeImmutable $date
+    ): self {
+
+        Assertion::notEmpty($name);
+        Assertion::notEmpty($description);
+        Assertion::notEmpty($author);
+
+        return self::occur(
+            $conferenceId,
+            [
+                'conferenceId' => (string) $conferenceId,
+                'name'         => $name,
+                'description'  => $description,
+                'author'       => $author,
+                'date'         => $date->format('U.u'),
+            ]
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function payload(): array
+    {
+        return [
+            'name'         => $this->name,
+            'description'  => $this->description,
+            'conferenceId' => (string) $this->conferenceId,
+            'author'       => $this->author,
+            'date'         => $this->date->format('U.u'),
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setPayload(array $payload): void
+    {
+        [
+            $this->name,
+            $this->description,
+            $this->author,
+            $this->conferenceId,
+            $this->date
+        ] = [
+            $payload['name'],
+            $payload['description'],
+            $payload['author'],
+            ConferenceId::fromString($payload['description']),
+            \DateTimeImmutable::createFromFormat('U.u', $payload['date']),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return ConferenceId
+     */
+    public function getConferenceId(): ConferenceId
+    {
+        return $this->conferenceId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
+    }
+}
