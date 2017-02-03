09f9e859f49f12ba3cf5bf170abc7e94092db330
Jefersson Nathan <admin@phpse.net>
2017-02-03 16:24:13 -0300

    added command handler factory for create conference

---
+++ src/Conference/Factory/CommandHandler/CreateConferenceHandlerFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conticket\Conference\Factory\CommandHandler;
+
+use Conference\Domain\Repository\ConferenceRepositoryInterface;
+use Conticket\Conference\Domain\CommandHandler\CreateConferenceHandler;
+use Interop\Container\ContainerInterface;
+
+/**
+ * @author Jefersson Nathan <malukenho@phpse.net>
+ */
+final class CreateConferenceHandlerFactory
+{
+    public function __invoke(ContainerInterface $container): CreateConferenceHandler
+    {
+        return new CreateConferenceHandler(
+            $container->get(ConferenceRepositoryInterface::class)
+        );
+    }
+}
