085523eaee1feff3e7eda675dbc44ae270acea8d
Jefersson Nathan <admin@phpse.net>
2017-02-17 11:52:07 -0300

    register missing services

--- config/services.php
+++ config/services.php
@@ -2,12 +2,16 @@
 
 declare(strict_types=1);
 
-use Conference\Domain\Repository\ConferenceRepositoryInterface;
-use Conference\Factory\Repository\ConferenceRepositoryFactory;
+use Conticket\Conference\Domain\Repository\ConferenceRepositoryInterface;
+use Conticket\Conference\Factory\Repository\ConferenceRepositoryFactory;
 use Conticket\Conference\Factory\CommandHandler\CreateConferenceHandlerFactory;
 use Conticket\Conference\Domain\Command\CreateConference;
 use Conticket\Conference\Infrastructure\Service\CommandBusFactory;
+use Conticket\Conference\Infrastructure\Service\ConnectionFactory;
+use Conticket\Conference\Infrastructure\Service\EventStoreFactory;
+use Doctrine\DBAL\Connection;
 use Interop\Container\ContainerInterface;
+use Prooph\EventStore\EventStore;
 use Prooph\ServiceBus\CommandBus;
 use Zend\Expressive\Application;
 use Zend\Expressive\Router\FastRouteRouter;
@@ -24,12 +28,25 @@ return (function () {
             },
 
             CommandBus::class => CommandBusFactory::class,
+            EventStore::class => EventStoreFactory::class,
+            Connection::class => ConnectionFactory::class,
 
             // @todo move commands/events to another config file
             CreateConference::class => CreateConferenceHandlerFactory::class,
 
             // @todo move repository to another file
             ConferenceRepositoryInterface::class => ConferenceRepositoryFactory::class,
-        ]
+
+            // @todo move db info to a class to get ENV vars
+            'db_dsn' => function () {
+                return 'mysql:host=localhost;dbname=conticket';
+            },
+            'db_user' => function () {
+                return 'root';
+            },
+            'db_password' => function () {
+                return 'root';
+            },
+        ],
     ];
 })();
