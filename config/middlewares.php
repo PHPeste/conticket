d4d4deb54ec87a01f62c3616725be89c2804adeb
Jefersson Nathan <admin@phpse.net>
2017-05-08 00:43:03 -0300

    create middleware to list conferences page

--- config/middlewares.php
+++ config/middlewares.php
@@ -2,13 +2,16 @@
 
 declare(strict_types=1);
 
+use Conticket\Conference\Factory\Middleware\ListConferencesMiddlewareFactory;
 use Conticket\Conference\Infrastructure\Middleware\CreateConferenceMiddleware;
 use Conticket\Conference\Factory\Middleware\CreateConferenceMiddlewareFactory;
+use Conticket\Conference\Infrastructure\Middleware\ListConferencesMiddleware;
 
 return (function () {
     return [
         'factories' => [
             CreateConferenceMiddleware::class => CreateConferenceMiddlewareFactory::class,
+            ListConferencesMiddleware::class => ListConferencesMiddlewareFactory::class,
         ],
     ];
 })();
--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Conticket\Conference\Infrastructure\Middleware\CreateConferenceMiddleware;
+use Conticket\Conference\Infrastructure\Middleware\ListConferencesMiddleware;
+use Zend\Expressive\Application;
 
 (function () {
     require __DIR__ . '/../vendor/autoload.php';
@@ -12,8 +14,10 @@ use Conticket\Conference\Infrastructure\Middleware\CreateConferenceMiddleware;
     /* @var $serviceManager \Zend\ServiceManager\ServiceManager */
     $serviceManager = require __DIR__ . '/../config/service-manager.php';
 
-    /* @var $app \Zend\Expressive\Application */
-    $app = $serviceManager->get(\Zend\Expressive\Application::class);
+    /* @var $app Application */
+    $app = $serviceManager->get(Application::class);
+
+    $app->get(ListConferencesMiddleware::PATH, ListConferencesMiddleware::class);
 
     // @todo change it to POST
     $app->get(CreateConferenceMiddleware::PATH, CreateConferenceMiddleware::class);
